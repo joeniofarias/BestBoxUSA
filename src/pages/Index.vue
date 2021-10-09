81e4c0e4f5bd7908bce872b0e7852c41a484290a
joeniofarias <joenio.farias@gmail.com>
2021-10-09 16:49:24 -0400

    ImagensRemovidas

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -30,7 +30,7 @@
     </div>
     <div class="main">
       <div class="section section-images">
-        <div class="container">
+        <!--<div class="container">
           <div class="row">
             <div class="col-xl">              
               <div class="hero-images-container">
@@ -42,13 +42,13 @@
                 <img src="img/eva.jpg" alt="" />
               </div>
             </div>
-            <!--<div class="col-md-5">              
+            <div class="col-md-5">              
               <div class="hero-images-container-2">
                 <img src="img/jfLogo.png" alt="" />
               </div> 
-            </div> !-->
+            </div>
           </div>
-        </div>
+        </div>!-->
       </div>
     </div>
     <basic-elements></basic-elements>
